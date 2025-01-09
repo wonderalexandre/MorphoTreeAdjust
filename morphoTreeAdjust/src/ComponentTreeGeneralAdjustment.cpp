5707daed4264d5fb0487dd5fbb1964bc310e84fd
Wonder Alexandre Luz Alves <wonderalexandre@gmail.com>
2025-01-09 09:08:40 -0300

    updates

--- morphoTreeAdjust/src/ComponentTreeGeneralAdjustment.cpp
+++ morphoTreeAdjust/src/ComponentTreeGeneralAdjustment.cpp
@@ -28,29 +28,46 @@ void ComponentTreeGeneralAdjustment::adjustMaxTree(ComponentTree &maxtree, std::
 	nodeContainsP->removeCNPs(flatZoneP); //line 4: ρ(Xa) := ρ(Xa) \ P
 	
 	//lines between 7 and 12
-    std::unordered_set<NodeCT*, NodeCT::NodeHashFunction> neighborNodesSet; 
-	neighborNodesSet.insert(nodeP);
+    std::unordered_set<NodeCT*, NodeCT::NodeHashFunction> neighborNodesSet1; //line 2: open
+	std::unordered_set<NodeCT*, NodeCT::NodeHashFunction> neighborNodesSet2;  //line3: close
 	for (int p : flatZoneP) {
 	    for (int q : adj->getAdjPixels(p)) {
-			if (a < maxtree.getSC(q)->getThreshold2() && b >= maxtree.getSC(q)->getThreshold2()) { 
-				neighborNodesSet.insert( maxtree.getSC(q) );
-		    }
+			if(maxtree.getSC(p)->getLevel() < maxtree.getSC(q)->getLevel()){  //elements of {q_i}
+				if (maxtree.getSC(q)->getThreshold2() <= b) { 
+					neighborNodesSet2.insert( maxtree.getSC(q) );
+				}else{
+					neighborNodesSet1.insert( maxtree.getSC(q) );
+				}
+			}
 	    }
 	}
 	auto comparator = [](NodeCT* a, NodeCT* b) { return a->getLevel() > b->getLevel(); };
-    std::priority_queue<NodeCT*, std::vector<NodeCT*>, decltype(comparator)> neighborNodes(comparator);
-	for (NodeCT* node : neighborNodesSet) {
-        neighborNodes.push(node);
+    std::priority_queue<NodeCT*, std::vector<NodeCT*>, decltype(comparator)> neighborNodes1(comparator);
+	for (NodeCT* node : neighborNodesSet1) {
+        neighborNodes1.push(node);
     }
 
+	std::priority_queue<NodeCT*, std::vector<NodeCT*>, decltype(comparator)> neighborNodes2(comparator);
+	for (NodeCT* node : neighborNodesSet2) {
+        neighborNodes2.push(node);
+    }
 
 	int w = -1; //line 13
 
 	//lines between 14 and 35
-	while (!neighborNodes.empty()) {
-		NodeCT* node = neighborNodes.top();
-		neighborNodes.pop();
-		std::cout << "neighborNodes: index: " << node->getIndex() << ", level: " << node->getLevel() << std::endl;
+	while (!neighborNodes1.empty()) {
+		NodeCT* node = neighborNodes1.top();
+		neighborNodes1.pop();
+		std::cout << "neighborNodes1: index: " << node->getIndex() << ", level: " << node->getLevel() << std::endl;
+	}
+
+	std::cout << "\n\n";
+
+	//lines between 14 and 35
+	while (!neighborNodes1.empty()) {
+		NodeCT* node = neighborNodes1.top();
+		neighborNodes1.pop();
+		std::cout << "neighborNodes2: index: " << node->getIndex() << ", level: " << node->getLevel() << std::endl;
 	}
 
 }
--- tests/testeComponentTreeGeneralAdjustment.cpp
+++ tests/testeComponentTreeGeneralAdjustment.cpp
@@ -34,18 +34,20 @@ int main()
 
     std::cout << "Iniciando...\n";
     
-    int numRows=9;
-    int numCols=9;
+    int numRows=10;
+    int numCols=17;
     int* img=new int[numRows * numCols]{
-        7, 7, 7, 7, 7, 7, 7, 7, 7,
-        7, 1, 1, 1, 1, 1, 7, 7, 7,
-        7, 1, 1, 1, 3, 1, 7, 7, 7,
-        7, 1, 1, 1, 1, 1, 7, 7, 7,
-        7, 7, 7, 7, 7, 7, 7, 7, 7,
-        7, 7, 7, 0, 0, 0, 0, 0, 7,
-        7, 7, 7, 0, 2, 0, 2, 0, 7,
-        7, 7, 7, 0, 0, 0, 0, 0, 7,
-        7, 7, 7, 7, 7, 7, 7, 7, 7};
+        6, 6, 6, 1, 1, 0, 1, 6, 6, 5, 2, 1, 1, 1, 0, 7, 7,
+        6, 1, 6, 1, 0, 0, 1, 6, 1, 5, 2, 2, 1, 1, 0, 3, 3,
+        6, 4, 0, 0, 6, 0, 1, 5, 5, 5, 1, 2, 1, 1, 0, 3, 3,
+        6, 1, 0, 6, 6, 6, 6, 0, 2, 1, 5, 3, 1, 1, 0, 5, 5,
+        6, 1, 0, 6, 0, 0, 6, 2, 2, 2, 8, 8, 1, 0, 1, 5, 5,
+        6, 1, 0, 6, 6, 6, 1, 2, 2, 2, 3, 8, 0, 0, 4, 1, 1,
+        1, 1, 3, 0, 0, 0, 7, 7, 3, 8, 8, 0, 4, 4, 1, 4, 4,
+        6, 3, 4, 3, 7, 0, 7, 6, 6, 6, 0, 2, 4, 1, 6, 1, 4,
+        6, 3, 4, 3, 7, 0, 2, 3, 3, 0, 2, 2, 4, 1, 5, 1, 4,
+        1, 3, 3, 3, 1, 0, 2, 3, 3, 0, 2, 2, 4, 1, 1, 1, 4
+    };
     
     int n = numRows * numCols;
     double radioAdj = 1.5;
@@ -55,7 +57,7 @@ int main()
     printTree(maxtree.getRoot());
     
 
-    int indexPixelFZ = 48;
+    int indexPixelFZ = 76; //particao do paper
     std::list<int> flatZone = maxtree.getSC(indexPixelFZ)->getCNPs(); //pixels de level 0
     std::cout << "\n\nflatZone de level:" << maxtree.getSC(indexPixelFZ)->getLevel() << "\n";
     
