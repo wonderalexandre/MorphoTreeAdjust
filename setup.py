829c9b37a72b8362004b731ee6db77497fdafc78
Wonder Alexandre Luz Alves <wonderalexandre@gmail.com>
2025-01-31 07:11:01 -0300

    update

--- setup.py
+++ setup.py
@@ -6,97 +6,136 @@ import platform
 import subprocess
 
 from pathlib import Path 
-
 from distutils.version import LooseVersion
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import setuptools
 
-class CMakeExtesion(Extension):
-  def __init__(self, name, sourcedir=""):
-    Extension.__init__(self, name, sources=[])
-    self.sourcedir = os.path.abspath(sourcedir)
+import shutil  # Adicione isso no topo do arquivo
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
 
 class CMakeBuild(build_ext):
-  def run(self):
-    try:
-      out = subprocess.check_output(["cmake", "--version"])
-    except:
-      raise RuntimeError("CMake must be installed to build the following extension: "
-        + ",".join(e.name for e in self.extensions))
-    
-    if platform.system() == "Windows":
-      cmake_version = LooseVersion(re.search(r"versions\s*([\d.]+)", out.decode()).group(1))
+    def run(self):
+        try:
+            out = subprocess.check_output(["cmake", "--version"])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extension: "
+                               + ", ".join(e.name for e in self.extensions))
 
-      if cmake_version < "3.1.0":
-        raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+        cmake_version = LooseVersion(re.search(r"version\s*([\d.]+)", out.decode()).group(1))
+        if platform.system() == "Windows" and cmake_version < "3.14":
+            raise RuntimeError("CMake >= 3.14 is required on Windows")
 
-    for ext in self.extensions:
-      self.build_extension(ext)
+        for ext in self.extensions:
+            self.build_extension(ext)
 
-  def build_extension(self, ext):
-    extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-    prefix = sysconfig.get_config_var("LIBDIR")
-    cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir, 
-                  "-DTYPE_EXECUTABLE=" + sys.executable,
-                  "-DPYTHON_LIBRARY_DIR={}".format(prefix)]
-    
-    self.extdir = extdir
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        prefix = sysconfig.get_config_var("LIBDIR")
 
-    cfg = "Debug" if self.debug else "Release"
-    build_args = ["--config", cfg]
+        # Argumentos do CMake
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "-DPYTHON_LIBRARY_DIR={}".format(prefix)
+        ]
 
-    if platform.system() == "Windows":
-      cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
-      if sys.maxsize > 2**32:
-        cmake_args += ["-A", "x64"]
-      build_args += ["--", "/m"]
-    else:
-      cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
-      build_args += ["--", "-j2"]
+        cfg = "Debug" if self.debug else "Release"
+        build_args = ["--config", cfg]
 
-    env = os.environ.copy()
-    env["CXXFLAGS"] = '{} - DVERSION_INFO=\\"{}\\"'.format(env.get("CXXFLAGS", ""), 
-      self.distribution.get_version())
+        # Configurações específicas para plataformas
+        if platform.system() == "Windows":
+            cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
+        else:
+            # Configurações para Linux/Unix
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
+            build_args += ["--", "-j2"]
 
-    if not os.path.exists(self.build_temp):
-      os.makedirs(self.build_temp)
-    
-    subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-    subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
-    self.move_output(ext)
-    print()
+        # Adicionar variáveis de ambiente para a compilação
+        env = os.environ.copy()
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get("CXXFLAGS", ""), self.distribution.get_version()).replace('"', '\\"')
 
-  def move_output(self, ext):
-    extdir = Path(self.extdir).resolve()
-    dest_path = Path(self.get_ext_fullpath(ext.name)).resolve()
-    source_path = extdir / self.get_ext_filename(ext.name)
-    dest_directory = dest_path.parents[0]
-    dest_directory.mkdir(parents=True, exist_ok=True)
-    self.copy_file(source_path, dest_path)
+        # Criar o diretório de build se não existir
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        # Executar o CMake
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=self.build_temp)
+
+        # Mover o output gerado para o local correto
+        self.move_output(ext)
+
+    def move_output(self, ext):
+        extdir = Path(self.build_lib).resolve()
+        dest_path = Path(self.get_ext_fullpath(ext.name)).resolve()
+        source_path = extdir / self.get_ext_filename(ext.name)
+        dest_directory = dest_path.parent
+
+        # Verificar se o arquivo foi gerado
+        if not source_path.exists():
+            raise RuntimeError(f"Arquivo de saída {source_path} não encontrado. Verifique se o CMake gerou corretamente o arquivo .so.")
+
+        # Criar diretório de destino se necessário e mover o arquivo
+        dest_directory.mkdir(parents=True, exist_ok=True)
+        self.copy_file(source_path, dest_path)
+
+        
+
+
+
+        
+# Verifica o sistema operacional atual
+system = platform.system()
+
+# Define a extensão do arquivo nativo com base no sistema operacional
+if system == "Linux":
+    native_ext = "*.so"
+elif system == "Darwin":
+    native_ext = "*.so"  # Para macOS, o Pybind gera .so, mas você pode usar *.dylib para bibliotecas dinâmicas
+elif system == "Windows":
+    native_ext = "*.dll"
+else:
+    raise RuntimeError(f"Plataforma {system} não suportada!")
+
+
 
 setup(
-  name="morphoTreeAdjust",
-  version="0.0.4",
-  description="MorphoTreeAdjust is a C++/Python implementation for adjusting the morpholofical trees.",
-  long_description="",
-  author="Wonder Alexandre Luz Alves",
-  author_email="worderalexandre@gmail.com",
-  license="GPL-3.0",
-  url= "https://github.com/wonderalexandre/ComponentTreeLearn",
-  #packages=["morphoTreeAdjust"],
-  keywords= "machine learning, morphological trees, mathematical morphology",
-  #include_package_data=True,
-  classifiers=[
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Science/Research",
-    "Topic :: Scientific/Engineering :: Image Processing",
-    "Programming Language :: Python"
-  ],
-  ext_modules=[CMakeExtesion(name="morphoTreeAdjust")],
-  cmdclass=dict(build_ext=CMakeBuild),
-  zip_safe=False
+    name="morphoTreeAdjust",
+    version="0.0.5",
+    description="MorphoTreeAdjust is a C++/Python implementation for adjusting the morpholofical trees.",
+    long_description="",
+    author="Wonder Alexandre Luz Alves",
+    author_email="worderalexandre@gmail.com",
+    license="GPL-3.0",
+    url="https://github.com/wonderalexandre/ComponentTreeLearn",
+    keywords="machine learning, morphological trees, mathematical morphology",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Programming Language :: Python",
+        "Programming Language :: C++",
+    ],
+    # Certificar-se de que as dependências necessárias estão instaladas
+    setup_requires=["setuptools", "wheel", "cmake>=3.14"],
+    ext_modules=[CMakeExtension('morphoTreeAdjust')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+    packages=["maf"],  # Definindo o pacote maf
+    package_dir={"maf": "python"},  # Atribuindo o diretório "python" ao pacote "maf"
+    package_data={
+        "maf": ["*.py", native_ext],  # Incluindo todos os arquivos Python no pacote e os modelos c++/pybinds
+    },
 )
+
 #send to pypi
 #1. python setup.py sdist
 #2. pipenv run twine upload dist/* -r pypi
