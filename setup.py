841c1457b7d3dcc6b14041a782b1c5b495492c19
Wonder Alexandre Luz Alves <wonderalexandre@gmail.com>
2025-09-13 08:21:06 -0300

    updates

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ else:
 
 setup(
     name="morphoTreeAdjust",
-    version="0.1.10",
+    version="0.1.19",
     description="MorphoTreeAdjust is a C++/Python implementation for adjusting the morpholofical trees.",
     long_description="",
     author="Wonder Alexandre Luz Alves",
